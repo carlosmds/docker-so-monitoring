3b3c3d8426794382b37e983cb59d268ab9e6cc46
Carlos <carlosmedeirosdesouza@gmail.com>
2021-01-13 18:51:50 -0300

    atualizado codigo da api e worker

--- app/index.js
+++ app/index.js
@@ -12,7 +12,30 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-    response.json({ info: 'API DE TESTE PARA TRABALHO DE SOP UDESC TADS' })
+
+    var request_number = request.query.request_number,
+        range = Array.from({length: parseInt(request_number)}),
+        response_array = Array.from({length: 0})
+    
+    for (const interator in range) {
+
+      var range2 = Array.from({length: interator})
+      
+      for (const interator2 in range2) {
+
+        var range3 = Array.from({length: interator2})
+
+        for (const interator3 in range3) {
+
+          let interator_calc = ((Math.floor(Math.random() * interator) + 1) * (Math.floor(Math.random() * interator2) + 1) * (Math.floor(Math.random() * interator3) + 1) * request_number)
+          response_array.push(interator_calc)
+        }
+      }  
+    }
+
+    let result = (response_array).reduce(function(total, num){ total += (num || 1); return total; }, 0);
+    
+    response.json({ info: result })
 })
 
 app.listen(port, () => {
--- worker/worker.py
+++ worker/worker.py
@@ -4,16 +4,17 @@ import os
 from time import sleep
 from random import randint
 
-numero_de_requisicoes = randint(10, 20)
+numero_de_requisicoes = 50
 ip_localhost = '192.168.0.14'
 
 print("aguardando inicialização do app_1...")
-sleep(10)
+sleep(5)
 print("iniciando loop de", numero_de_requisicoes, "requisições...")
 
 for requisicao_numero in range(numero_de_requisicoes):
-    request = requests.get(''.join(["http://", ip_localhost, ":3000/"]))
-    print("requisição nº ", requisicao_numero, "status code: ", request.status_code)
+    request_number = requisicao_numero + 1
+    request = requests.get(''.join(["http://", ip_localhost, ":3000/"]), params={'request_number': (request_number+1)})
+    print("requisição nº", request_number, "status code:", request.status_code, "response: ", request.text)
     sleep(1)
 
-print('PRONTO PARA DALHE')
+print('encerrando worker...')
